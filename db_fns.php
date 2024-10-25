f7a43f7d6d2ae84340955a84eb61f0951c2c59ef
Valenangelina <144827098+Valenangelina@users.noreply.github.com>
2024-10-25 20:23:52 +0700

    db_fns.php

--- db_fns.php
+++ db_fns.php
@@ -1,11 +1,11 @@
 <?php
 function db_connect() {
     $host = 'localhost';
-    $user = 'todo_user';
-    $password = '';
-    $dbname = 'todo_app';
+    $user = 'hary8495_alenajadeh';
+    $password = 'alenajadeh';
+    $dbname = 'hary8495_todo_app';
 
-    $conn = new mysqli('localhost', 'todo_user', 'webpro1000', 'todo_app');
+    $conn = new mysqli('localhost', 'hary8495_alenajadeh', 'alenajadeh', 'hary8495_todo_app');
     
     if ($conn->connect_error) {
         die('Connection failed: ' . $conn->connect_error);
